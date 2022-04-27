316810e081b01378ab074ee1e749d1aeafd16317
Aurore-Grimault <auroregrimault@hotmail.fr>
2022-04-27 22:40:02 +0200

    commit 0.1

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,57 @@
-<?php  echo'<a href="includes\head.inc.html" > Mon lien </a>'; ?>
-<body>
-    <?php  echo'<a href="includes\header.inc.html" > Mon lien </a>'; ?>
-</body>
-<?php  echo'<a href="includes\body.inc.html" > Mon lien </a>'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="Activité PHP promo SIMPLON Boulogne sur Mer" />
+        <meta name="keywords" content="HTML, CSS, PHP, Bootstrap, Bootswatch, SIMPLON, Boulogne" />
+        <meta name="author" content="Nicolas HERBEZ" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
 
-Bonjour, <?php echo htmlspecialchars($_POST['nom']); ?>.
-Tu as <?php echo (int)$_POST['age']; ?> ans.
+        <title>PHP - Procédural</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/spacelab/bootstrap.min.css">  
+    </head>
+    
+    <header class="d-none d-sm-block">
+        <div class="card bg-dark rounded-0">
+        <div class="row g-0">
+
+            <div class="col-md-5">
+                <img src="images\php.png" alt="logo php" class="img-fluid rounded-start m-3">
+            </div>
+
+            <div class="col-md-7">
+                <div class="card-body text-white">
+                    <h1 class="card-title">PHP procédural</h1>
+                    <p class="card-text">Créer le back-end permettant de sauvegarder des données en session à travers un formulaire 🙂</p>
+                    <p class="card-text"><small class="text-muted">J'apprends et je me perfectionne</small></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="p-2 bg-dark mt-2"></div>
+    <div class="p-1 bg-dark mt-2 mb-3"></div>
+
+    </header>
+    
+    <body>
+        <form action="action.php" method="post">
+            <p>Votre nom : <input type="text" name="nom" /></p>
+            <p>Votre âge : <input type="text" name="age" /></p>
+            <p><input type="submit" value="Ajouter des données"></p>
+            <button type="submit" name="btn_vote">Voter</button>          
+       
+        </form>
+    </body>
+    
+     <!-- Le pied de page -->
+    <footer id="pied_de_page">
+        <p> &copy; Aurore Grimault - <a href="https://github.com/Aurore-Grimault"> Github </a> 
+        </p>
+    </footer>
+    
+   
+</html>

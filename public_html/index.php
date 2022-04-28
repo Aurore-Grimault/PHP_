1452247f66d90347eb311af001ce56aa45553952
Aurore-Grimault <auroregrimault@hotmail.fr>
2022-04-28 19:28:28 +0200

    commit 0.4

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,7 @@
+<?php session_start();
+    if(isset($_SESSION['table'])) $table = $_SESSION['table']; 
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <?php include_once './includes/head.inc.html'; ?>
@@ -8,19 +12,17 @@
         <div class="container">
                 <div class="row">
                     <nav class="col-md-3 mt-3">
-                        <a class="btn btn-outline-secondary w-50" role="button" href="index.php"> Home </a> 
-                        <?php if(isset($table)) include_once './includes/form.inc.html'; ?>
+                        <a class="btn btn-outline-secondary w-50" role="button" href="./index.php"> Home </a> 
+                        <?php if(isset($table)) include_once './includes/ul.inc.php'; ?>
                     </nav>
 
                     <section class="col-md-9 mt-3">
                         <a class="btn btn-primary" role="button" href="index.php?add"> Ajouter des données </a> 
                         <?php echo $_GET["add"]?>
                         <?php if (isset($_GET["add"])) include_once './includes/form.inc.html' ; ?> 
-                    
                     </section>
 
-                </div>    
-           
+                </div>   
        
         </div>
     
